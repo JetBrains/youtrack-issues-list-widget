d93f3a26b26bcff19b9102f783f8adea47e82a4a
Andrey Skladchikov <4318513+andrey-skl@users.noreply.github.com>
2023-11-01 11:40:15 +0100

    JT-72071 set default issues refresh period to 10m [publish]

--- src/app/issues-list-widget.js
+++ src/app/issues-list-widget.js
@@ -16,6 +16,7 @@ class IssuesListWidget extends React.Component {
 
   static COUNTER_POLLING_PERIOD_SEC = 240; // eslint-disable-line no-magic-numbers
   static COUNTER_POLLING_PERIOD_MLS = 60000; // eslint-disable-line no-magic-numbers
+  static DEFAULT_REFRESH_PERIOD_SEC = 240; // eslint-disable-line no-magic-numbers
 
   static digitToUnicodeSuperScriptDigit = digitSymbol => {
     const unicodeSuperscriptDigits = [
@@ -101,7 +102,7 @@ class IssuesListWidget extends React.Component {
     this.state = {
       isConfiguring: false,
       isLoading: true,
-      refreshPeriod: IssuesListWidget.COUNTER_POLLING_PERIOD_SEC
+      refreshPeriod: IssuesListWidget.DEFAULT_REFRESH_PERIOD_SEC
     };
     this.missedUpdate = false;
 
